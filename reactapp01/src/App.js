8856c90340802c5e6b7835b0efd7cd9ba6fe246b
Pedro Egydio <pedroe01@hotmail.com>
2023-04-13 22:23:14 -0300

    Add routes to project

--- reactapp01/src/App.js
+++ reactapp01/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Sobre from './Sobre';
+import Teste from './Teste';
+import Header from './Header';
+import TesteApi from './TesteApi';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to='/sobre'>Sobre</Link> <br></br>
+        <Link to='/teste'>Teste</Link> <br></br>
+        <Link to='/header'>Header</Link> <br></br>
+        <Link to='/apirequest'>Request API</Link> <br></br>
       </header>
+      <main>
+        <Routes>
+            <Route path='/sobre' element={<Sobre/>}/>
+            <Route path='/teste' element={<Teste/>}/>
+            <Route path='/header' element={<Header/>}/>
+            <Route path='/apirequest' element={<TesteApi/>}/>
+        </Routes>
+    </main>
     </div>
   );
 }
--- reactapp01/src/index.js
+++ reactapp01/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
-import Teste from './Teste';
-import Header from './Header';
-import RotaTest from './RotaTest';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Header />
+  <BrowserRouter>
     <App />
-    <Teste />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
