beb34ea127300aab9aa407d8d29d03ade6e18a34
Pedro Egydio <pedroe01@hotmail.com>
2023-05-03 19:31:56 -0300

    Refactor README and Add AC3

--- reactapp01/src/App.js
+++ reactapp01/src/App.js
@@ -2,6 +2,7 @@ import Sobre from './Sobre';
 import Teste from './Teste';
 import Header from './Header';
 import TesteApi from './TesteApi';
+import ConsumeFlaskApi from './ConsumeFlaskApi'
 import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Link to='/teste'>Teste</Link> <br></br>
         <Link to='/header'>Header</Link> <br></br>
         <Link to='/apirequest'>Request API</Link> <br></br>
+        <Link to='/flaskapi'>Request Flask API</Link> <br></br>
       </header>
       <main>
         <Routes>
@@ -19,6 +21,7 @@ function App() {
             <Route path='/teste' element={<Teste/>}/>
             <Route path='/header' element={<Header/>}/>
             <Route path='/apirequest' element={<TesteApi/>}/>
+            <Route path='/flaskapi' element={<ConsumeFlaskApi/>}/>
         </Routes>
     </main>
     </div>
